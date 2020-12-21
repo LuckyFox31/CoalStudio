50bb3953d6e7d2b2827aa737461c498b5ddaebc6
LuckyFox <hello@luckyfox.fr>
2020-12-21 18:10:31 +0100

    update os detection index

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,13 @@
-const game = "skylife";
-const extension = "dmg";
+import {list} from "./list.js";
 
 const button = document.querySelector("#download");
 
 const os = platform.os.family;
-if (os == "OS X") {
-    button.href = `assets/games/skylife/macos/${game}.${extension}`;
+if(list[0].system.windows){
+    button.href = `assets/games/${list[0].name}/windows/${list[0].name}.${list[0].extension.windows}`;
+}
+if(list[0].system.mac){
+    if (os == "OS X") {
+        button.href = `assets/games/${list[0].name}/macos/${list[0].name}.${list[0].extension.mac}`;
+    }
 }
