2c7de0fbb66bfb72c7ccc9f0a6a4cb6a98f81a18
Nirbose <62440475+Nirbose@users.noreply.github.com>
2021-02-16 23:56:06 +0100

    Update_1.3
    Correction de fonction dans Community.php + ajout d'une nouvelle base pour une fonction edite_user.

--- app/Panel/Community/Community.php
+++ app/Panel/Community/Community.php
@@ -8,52 +8,10 @@ use App\Database\DBConnexion;
 class Community {
 
     private $user;
+    public $reply;
 
     public function __construct($user) {
         $this->user = $user;
-    }
-
-    /**
-     * game_add function
-     *
-     * @param string $name
-     * @param string $desc
-     * @param string $img
-     * @param url $video
-     * @param string $mature
-     * @param string $game_file
-     * @param int $version
-     * @param string $credits
-     * @return void
-     */
-    public function game_add($name, $desc, $img, $video = null, $mature, $game_file, $version, $credits) {
-        
-        $form = new FormControlleur;
-        $name_game = $form->pseudo($name);
-        $desc_game = htmlspecialchars($desc);
-
-        $img_game = str_replace(",", ".", $img);
-
-        // foreach($img as $i) {
-        //     echo $i;
-        // }
-    }
-
-    /**
-     * game_listing function
-     *
-     * @return void
-     * 
-     * utilisation de while : 
-     * while($g = $game_listing()->fetch()) {
-     *      echo ' ok';
-     *  }
-     */
-    public function game_listing() {
-
-        $bdd = new DBConnexion(DB_NAME);
-        $games = $bdd->getPdo()->query("SELECT * FROM game ORDER BY id DESC");
-
     }
 
     /** User function */
@@ -66,7 +24,7 @@ class Community {
      * @param string $pass
      * @return void
      */
-    public function create_user($pseudo) {
+    public function create_user(string $pseudo) {
 
         $form = new FormControlleur;
         $bdd = new DBConnexion(DB_NAME);
@@ -76,10 +34,10 @@ class Community {
         $user_pass_hash = $form->password($user_pass);
         $user_token = sha1($user_pass);
 
-        $insert = $bdd->getPdo()->prepare("INSERT INTO user(pseudo, pass, admin, token, created_at) VALUE(?, ?, 1, ?, NOW())");
+        $insert = $bdd->getPdo()->prepare("INSERT INTO user(pseudo, pass, admin, token, created_at) VALUE(?, ?, 0, ?, NOW())");
         $insert->execute([$user_pseudo, $user_pass_hash, $user_token]);
 
-        echo "User Enregistrais ! \n mot de passe : $user_pass ";
+        $this->reply = "User Enregistrais ! \n mot de passe : $user_pass ";
 
     }
 
@@ -89,24 +47,22 @@ class Community {
      * @param int $user_id
      * @return void
      */
-    public function delete_user($user_id) {
+    public function delete_user(int $user_id) {
         $bdd = new DBConnexion(DB_NAME);
 
         $delete = $bdd->getPdo()->prepare("DELETE FROM user WHERE id = ?");
         $delete->execute([$user_id]);
 
-        echo "supprimer avec succes !";
+        $this->reply = "supprimer avec succes !";
     }
 
     /**
-     * list_user function
+     * Edite de compte
      *
+     * @param integer $user_id
      * @return void
      */
-    // public function list_user() {
-    //     $bdd = new DBConnexion(DB_NAME);
-
-    //     return $query = $bdd->getPdo()->query("SELECT * FROM user ORDER BY id DESC");
-
-    // }
+    public function edit_user(int $user_id) {
+        // Edite de compte prochainement.
+    }
 }
